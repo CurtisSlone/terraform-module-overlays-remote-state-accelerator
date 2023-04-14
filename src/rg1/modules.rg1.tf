19e84c5075f4001a82fdec0c844fba79d7da75ce
Curtis Slone <curtisdslone@gmail.com>
2023-04-14 12:07:53 +0200

    Actions and examples testing

---
+++ src/rg1/modules.rg1.tf
@@ -0,0 +1,19 @@
+module "mod_rg1" {
+    source = "azurenoops/overlays-resource-group/azurerm"
+    version = "~> 1.0"
+
+    //Resource Group Configuration
+    location = var.location
+    org_name = var.org_name
+    environment = var.environment
+    workload_name = var.workload_name
+    use_location_short_name = var.use_location_short_name
+    custom_rg_name = var.custom_resource_group_name != null ? var.custom_resource_group_name : null
+
+    //Resource Locks
+    enable_resource_locks = var.enable_resource_locks
+    lock_level = var.lock_level
+
+    //Tags
+    add_tags = var.tags 
+}
